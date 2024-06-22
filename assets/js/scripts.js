f2d526455431a36d7052cd7661bde67e3378bb58
Gustavo <envolti.gustavo.peron@redeunifique.com.br>
2024-06-22 08:15:02 -0300

    .

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var slider = new KeenSlider("#carousel-icons", {
+  var slider1 = new KeenSlider("#carousel-icons", {
     loop: false,
     mode: "free",
     slides: {
       perView: 4.5,
+      spacing: 14,
+    },
+  });
+
+  var slider2 = new KeenSlider("#carousel-textos", {
+    loop: false,
+    mode: "free",
+    slides: {
+      perView: 1.5,
       spacing: 14,
     },
   });
